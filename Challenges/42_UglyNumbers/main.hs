7919efe49c21f8070077f525daf934f617b6e783
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-02-24 22:56:21 +0100

    Optimized solution for challenge 42

--- Challenges/42_UglyNumbers/main.hs
+++ Challenges/42_UglyNumbers/main.hs
@@ -2,33 +2,48 @@ module Main where
 
 import System.Environment (getArgs)
 
+type Expression = [String]
+
 isUglyNumber :: Int -> Bool
 isUglyNumber n =
     n `divBy` 2 || n `divBy` 3 || n `divBy` 5 || n `divBy` 7
     where x `divBy` y = x `mod` y == 0
 
-evaluateExpression' :: Int -> [String] -> Int
+evaluateExpression' :: Int -> Expression -> Int
 evaluateExpression' acc [] = acc
 evaluateExpression' acc ("+":x:xs) = evaluateExpression' (acc + read x) xs
 evaluateExpression' acc ("-":x:xs) = evaluateExpression' (acc - read x) xs
 evaluateExpression' _ _ = error "Invalid Expression"
 
-evaluateExpression :: [String] -> Int
+evaluateExpression :: Expression -> Int
 evaluateExpression [] = error "Invalid Expression"
 evaluateExpression (x:xs) = evaluateExpression' (read x) xs
 
-makeExpressions :: String -> [[String]]
+addExpressions :: Char -> [Expression] -> [Expression]
+addExpressions _ [] = []
+addExpressions _ ([]:_) = error "Invalid Expression"
+addExpressions c (e@(cs:es):exps) =
+    ([c]:"+":e) : ([c]:"-":e) : ((c:cs):es) : (addExpressions c exps)
+
+makeExpressions :: String -> [Expression]
 makeExpressions [] = []
 makeExpressions [c] = [[[c]]]
-makeExpressions (c1:c2:cs) =
-    let subExpressions = makeExpressions (c2:cs)
-    in  [ [c1]:"+":cs' | cs' <- subExpressions ] ++
-        [ [c1]:"-":cs' | cs' <- subExpressions ] ++
-        [ (c1:c):cs' | (c:cs') <- subExpressions ]
+makeExpressions (c:cs) =
+    let subExpressions = makeExpressions cs
+    in  addExpressions c subExpressions
+
+simplifyDigits :: String -> (Int, String)
+simplifyDigits s =
+    let (zeros, remainder) = span (== '0') (init s)
+    in  (length zeros, remainder ++ [last s])
 
 countUglyExpressions :: String -> Int
-countUglyExpressions =
-    length . filter isUglyNumber . map evaluateExpression . makeExpressions
+countUglyExpressions s =
+    let (factor, simpString) = simplifyDigits s
+        exps = makeExpressions simpString
+        evaluatedExps = map evaluateExpression exps
+        uglyExps = length $ filter isUglyNumber evaluatedExps
+    in  3^factor * uglyExps
 
 processLine :: String -> String
 processLine = show . countUglyExpressions
