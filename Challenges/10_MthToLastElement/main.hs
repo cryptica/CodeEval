ff685f765d3b22a713ef68b3383a26fab0e95531
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-02-26 20:14:11 +0100

    Solved challenge 10

---
+++ Challenges/10_MthToLastElement/main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import System.Environment(getArgs)
+import Control.Applicative
+
+processLine :: String -> Maybe String
+processLine line =
+    let (ms:list) = reverse $ words line
+        m = read ms - 1
+    in  if m < length list then Just (list !! m) else Nothing
+
+
+maybeIO :: (a -> IO ()) -> Maybe a -> IO ()
+maybeIO _ Nothing = return ()
+maybeIO f (Just x) = f x
+
+main :: IO ()
+main = do
+    [inputFile] <- getArgs
+    input <- readFile inputFile
+    mapM_ (maybeIO putStrLn) $ map processLine $ lines input
+
