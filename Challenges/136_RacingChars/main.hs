59453530e2fa6da9ffd70d1d681542226ea46459
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-03-21 22:33:34 +0100

    Simplified and optimized solution for challenge 136

--- Challenges/136_RacingChars/main.hs
+++ Challenges/136_RacingChars/main.hs
@@ -9,21 +9,19 @@ replaceChar s i c =
     let (before,_:after) = splitAt i s
     in  before ++ [c] ++ after
 
-race' :: [String] -> Int -> Maybe (Int, [String])
-race' [] _ = Just (0, [])
-race' (x:xs) pos = max (goTo '_' 0) (goTo 'C' 1)
-    where goTo c inc = do
-            pos' <- elemIndex c x
-            if pos < 0 || abs (pos' - pos) <= 1 then do
-                let sym = if pos < 0 || pos' == pos then '|'
-                          else if pos' < pos then '/'
-                          else '\\'
-                (score, track) <- race' xs pos'
-                return (score + inc, (replaceChar x pos' sym):track)
-            else Nothing
+race' :: [String] -> Int -> [String]
+race' [] _ = []
+race' (x:xs) pos =
+    let pos' = case elemIndex 'C' x of
+            Just p -> p
+            Nothing -> fromJust $ elemIndex '_' x
+        sym = if pos < 0 || pos' == pos then '|'
+              else if pos' < pos then '/'
+              else '\\'
+    in  (replaceChar x pos' sym):(race' xs pos')
 
 race :: [String] -> [String]
-race xs = snd $ fromJust $ race' xs (-1)
+race xs = race' xs (-1)
 
 processInput :: String -> String
 processInput = unlines . race . lines
