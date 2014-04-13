75741a84f537c9ecc7e80aa9faa2f822443d7533
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-04-13 15:41:40 +0200

    Optimized solution for challenge 139

--- Challenges/139_WorkingExperience/main.hs
+++ Challenges/139_WorkingExperience/main.hs
@@ -3,31 +3,27 @@ module Main where
 import System.Environment(getArgs)
 import Control.Monad (liftM)
 import Data.List (sort)
+import Data.Maybe (fromJust)
+import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
-           deriving (Show, Read, Eq, Ord, Enum, Bounded)
+monthMap :: M.Map T.Text Int
+monthMap = M.fromList $ zip (map T.pack
+        ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]) [1..]
 
-data Date = Date { year :: Int, month :: Month } deriving (Show, Eq, Ord, Bounded)
-
-instance Enum Date where
-        fromEnum d = year d * 12 + fromEnum (month d)
-        toEnum d = let (y,m) = d `divMod` 12
-                   in  Date { year=y, month=toEnum m }
+type Date = Int
 
 getWorkingExperience :: [(Date,Date)] -> Int
-getWorkingExperience xs = months `div` 12
-        where
-            months = snd $ foldl addInterval (minBound :: Date, 0) $ sort xs
-            addInterval (dEnd,time) (d1,d2) =
+getWorkingExperience = (`div` 12) . snd . foldl addInterval (minBound, 0) . sort
+        where addInterval (dEnd,time) (d1,d2) =
                   (max dEnd d2, time + timeFromInterval(max (succ dEnd) d1, max dEnd d2))
-            timeFromInterval (d1,d2) = (fromEnum d2 - fromEnum d1) + 1
+              timeFromInterval (d1,d2) = (fromEnum d2 - fromEnum d1) + 1
 
 parseDate :: T.Text -> Date
 parseDate t =
         let [m,y] = T.split (==' ') t
-        in  Date { month=read (T.unpack m), year=read (T.unpack y) }
+        in  fromJust (M.lookup m monthMap) + 12 * read (T.unpack y)
 
 parseInterval :: T.Text -> (Date,Date)
 parseInterval t =
